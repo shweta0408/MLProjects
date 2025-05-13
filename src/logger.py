e98b95d55a0539ba99e29180f6499b6012b81439
Shweta0408 <shmis2004@gmail.com>
2025-05-14 01:35:36 +0530

    model and eda

--- src/logger.py
+++ src/logger.py
@@ -2,25 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# Create logs directory if it doesn't exist
-LOG_DIR = "logs"
-os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
 
-# Generate unique log file name with timestamp
-LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(levelname)s - %(message)s",
-    level=logging.INFO
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
 )
-
-# Optionally, also log to console
-console = logging.StreamHandler()
-console.setFormatter(logging.Formatter(
-    "[%(asctime)s] %(levelname)s - %(message)s"))
-logging.getLogger().addHandler(console)
-
-logger = logging.getLogger(__name__)
